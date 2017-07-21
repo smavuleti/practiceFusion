7ed4104844766c8ca5200762e41360eb8105acb1
dattadh <hariphaneendra.dattada@lifetech.com>
2017-07-20 19:08:59 -0700

    first commit-check2

--- server.js
+++ server.js
@@ -8,13 +8,11 @@ var port = process.env.PORT || 8080;
 //set the view engine to ejs
 app.set('view engine', 'ejs');
 
-//make express look in the public directory for assets
-app.use(express.static(__dirname+'/public'));
 
 //set the home page route
 app.get('/', function(req,res){
-	//ejs render automatically looks in the views folder
-	res.render('index');
+	//ejs render automatically looks in the folder
+	res.sendfile('index.html');
 
 });
 
